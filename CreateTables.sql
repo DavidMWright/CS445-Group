b714369dfeb0809200ebef0ed7377c00d37cd2f3
David Wright <wrig6296@eniac.cs.pacificu.edu>
2019-11-11 17:22:00 -0800

    Create Table Done

--- CreateTables.sql
+++ CreateTables.sql
@@ -21,26 +21,45 @@ CREATE TABLE Bettors
     
     Balance INT DEFAULT 0,
 
-    CONSTRAINT Bettors_BettorID_PK PRIMARY KEY (BettorID)
+    CONSTRAINT Bettors_FK FOREIGN KEY (BettorID) REFERENCES Users(UserID)
 ) Engine=InnoDB;
 
 
 CREATE TABLE Admins
 (
-    AdminID INT NOT NULL AUTO_INCREMENT,
+    AdminID INT NOT NULL,
 
-    CONSTRAINT Admins_AdminID_PK PRIMARY KEY (AdminID)
+    CONSTRAINT Admins_PK FOREIGN KEY (AdminID) REFERENCES Users(UserID)
 ) Engine=InnoDB;
 
 
 CREATE TABLE Bet
 (
-    BetID INT NOT NULL,
+    BetID INT NOT NULL AUTO_INCREMENT,
 
     CurrentOdds INT,
 
     CONSTRAINT Bet_BetID_PK PRIMARY KEY (BetID)
 ) Engine=InnoDB;
+
+
+CREATE TABLE Logo 
+(
+	LogoID INT NOT NULL AUTO_INCREMENT,
+	Image LONGBLOB NOT NULL,
+	
+	CONSTRAINT Logo_LogoID_PK PRIMARY KEY (LogoID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+
+CREATE TABLE SportsTeams (
+	SportsTeamID INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR (200) NOT NULL,
+	LogoID INT NOT NULL,
+	
+	CONSTRAINT SportsTeams_SportsTeamID_PK PRIMARY KEY (SportsTeamID),
+	CONSTRAINT SportsTeams_LogoID_FK FOREIGN KEY (LogoID)  REFERENCES Logo (LogoID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
 
 
 CREATE TABLE MadeBet
@@ -56,10 +75,23 @@ CREATE TABLE MadeBet
 
     CONSTRAINT MadeBet_PK PRIMARY KEY (BetID, BettorID, HomeID, AwayID),
     CONSTRAINT MadeBet_BetID_FK FOREIGN KEY (BetID) REFERENCES Bet(BetID),
-    CONSTRAINT MadeBet_BettorID_FK FOREIGN KEY (BettorID) REFERENCES Bettor(BettorID),   
+    CONSTRAINT MadeBet_BettorID_FK FOREIGN KEY (BettorID) REFERENCES Bettors(BettorID),   
     CONSTRAINT MadeBet_HomeID_FK FOREIGN KEY (HomeID) REFERENCES SportsTeams(SportsTeamID),
     CONSTRAINT MadeBet_AwayID_FK FOREIGN KEY (AwayID) REFERENCES SportsTeams(SportsTeamID)
 ) Engine=InnoDB;
+
+
+CREATE TABLE Players (
+	PlayerID INT NOT NULL AUTO_INCREMENT,
+	FName VARCHAR (50) NOT NULL,
+	LName VARCHAR (50) NOT NULL,
+	Height VARCHAR (50) NOT NULL,
+	Weight INT NOT NULL,
+	OnA INT NOT NULL,
+	
+	CONSTRAINT Players_PlayerID_PK PRIMARY KEY (PlayerID),
+	CONSTRAINT Players_OnA_FK FOREIGN KEY (OnA) REFERENCES SportsTeams (SportsTeamID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
 
 
 CREATE TABLE Win
@@ -117,11 +149,11 @@ CREATE TABLE Cards
 
     CONSTRAINT Cards_PK PRIMARY KEY (BetID),
 
-    CONSTRAINT Cards_BetID_FK FOREIGN KEY (BetID) REFERENCES Bet(BetID),
+    CONSTRAINT Cards_BetID_FK FOREIGN KEY (BetID) REFERENCES Bet(BetID)
 ) Engine=InnoDB;
 
 
-CREATE TABLE Starting
+CREATE TABLE Starters
 (
     BetID INT NOT NULL,
     TeamID INT NOT NULL,
@@ -141,15 +173,102 @@ CREATE TABLE Starting
 
     CONSTRAINT Starting_BetID_FK FOREIGN KEY (BetID) REFERENCES Bet(BetID),
     CONSTRAINT Starting_TeamID_FK FOREIGN Key (TeamID) REFERENCES SportsTeams(SportsTeamID),
-    CONSTRAINT Starting_P1ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P2ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P3ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P4ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P5ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P6ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P7ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P8ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P9ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P10ID_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID),
-    CONSTRAINT Starting_P1I1D_FK FOREIGN Key (PlayerID) REFERENCES Players(PlayerID)
+    CONSTRAINT Starting_P1ID_FK FOREIGN Key (P1ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P2ID_FK FOREIGN Key (P2ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P3ID_FK FOREIGN Key (P3ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P4ID_FK FOREIGN Key (P4ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P5ID_FK FOREIGN Key (P5ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P6ID_FK FOREIGN Key (P6ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P7ID_FK FOREIGN Key (P7ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P8ID_FK FOREIGN Key (P8ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P9ID_FK FOREIGN Key (P9ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P10ID_FK FOREIGN Key (P10ID) REFERENCES Players(PlayerID),
+    CONSTRAINT Starting_P1I1D_FK FOREIGN Key (P11ID) REFERENCES Players(PlayerID)
 ) Engine=InnoDB;
+
+
+CREATE TABLE League 
+(
+	LeagueID INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR (200) NOT NULL,
+	
+	CONSTRAINT League_LeagueID_PK PRIMARY KEY (LeagueID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+
+CREATE TABLE ParticipatesIn 
+(
+	SportsTeamID INT NOT NULL,
+	LeagueID INT NOT NULL,
+	Standing INT NOT NULL,
+	
+	CONSTRAINT ParticipatesIn_PK PRIMARY KEY (SportsTeamID, LeagueID),
+	CONSTRAINT ParticipatesIn_SportsTeamID_FK FOREIGN KEY (SportsTeamID) REFERENCES SportsTeams (SportsTeamID),
+	CONSTRAINT ParticipatesIn_League_FK FOREIGN KEY (LeagueID) REFERENCES League (LeagueID)
+
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+
+CREATE TABLE Positions 
+(
+	PosID INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR (200) NOT NULL,
+	Abbreviation VARCHAR (10) NOT NULL,
+	
+	CONSTRAINT Positions_PosID_PK PRIMARY KEY (PosID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+
+CREATE TABLE HasAPosition 
+(
+	PlayerID INT NOT NULL,
+	PosID INT NOT NULL,
+	
+	CONSTRAINT HasAPosition_PK PRIMARY KEY (PlayerID, PosID),
+	CONSTRAINT HasAPosition_PlayerID_FK FOREIGN KEY (PlayerID) REFERENCES Players (PlayerID),
+	CONSTRAINT HasAProfile_PosID_FK FOREIGN KEY (PosID) REFERENCES Positions (PosID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+
+CREATE TABLE Matchs (
+	HomeSportsTeamID INT NOT NULL,
+	AwaySportsTeamID INT NOT NULL,
+	ShotsOnGoalHome INT,
+	ShotsOnGoalAway INT,
+	FinalScoreHome INT,
+	FinalScoreAway INT,
+	DateOfMatch DATE,
+	
+	CONSTRAINT Match_PK PRIMARY KEY (HomeSportsTeamID, AwaySportsTeamID),
+	CONSTRAINT Match_AwaySportsTeamID_FK FOREIGN KEY (AwaySportsTeamID)  REFERENCES SportsTeams (SportsTeamID),
+	CONSTRAINT Match_HomeSportsTeamID_FK FOREIGN KEY (HomeSportsTeamID)  REFERENCES SportsTeams (SportsTeamID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+
+CREATE TABLE Starts 
+(
+	PlayerID INT NOT NULL,
+	HomeSportsTeamID INT NOT NULL,
+	AwaySportsTeamID INT NOT NULL,
+	
+	CONSTRAINT Starts_PK PRIMARY KEY (PlayerID, HomeSportsTeamID, AwaySportsTeamID),
+	CONSTRAINT Starts_PlayerID_FK FOREIGN KEY (PlayerID) REFERENCES Players (PlayerID),
+	CONSTRAINT Starts_HomeSportsTeamID_FK FOREIGN KEY (HomeSportsTeamID) REFERENCES Matchs (HomeSportsTeamID),
+	CONSTRAINT Starts_AwaySportsTeamID_FK FOREIGN KEY (AwaySportsTeamID) REFERENCES Matchs (AwaySportsTeamID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+
+CREATE TABLE MatchStats (
+	PlayerID INT NOT NULL,
+	HomeSportsTeamID INT NOT NULL,
+	AwaySportsTeamID INT NOT NULL,
+	NumCards INT,
+	NumGoals INT, 
+	NumAssists INT,
+	
+	CONSTRAINT MatchStats_PK PRIMARY KEY (PlayerID, HomeSportsTeamID, AwaySportsTeamID),
+	CONSTRAINT MatchStats_PlayerID_FK FOREIGN KEY (PlayerID) REFERENCES Players (PlayerID),
+	CONSTRAINT MatchStats_HomeSportsTeamID_FK FOREIGN KEY (HomeSportsTeamID) REFERENCES Matchs (HomeSportsTeamID),
+	CONSTRAINT MatchStats_AwaySportsTeamID_FK FOREIGN KEY (AwaySportsTeamID) REFERENCES Matchs (AwaySportsTeamID)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
+
