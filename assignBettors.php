39b42cb4376d39b40523617422ac2d86346320ac
DavidMWright <wrig6296@pacificu.edu>
2019-11-17 20:29:01 -0800

    PHP script to add all non admin users to bettors

---
+++ assignBettors.php
@@ -0,0 +1,48 @@
+<?php 
+	require_once(__DIR__ . '/../basicErrorHandling.php');
+	require_once ('connDB.php');
+?>
+
+
+<html>
+	<body>
+		<?php
+			$dbh=db_connect();
+			
+			$rows = array();
+
+			$sth = $dbh -> prepare("SELECT UserID FROM Users LEFT JOIN Admins ON Users.UserID = Admins.AdminID");
+			// run the query
+			$sth -> execute();
+			
+			while ($row = $sth -> fetch())
+			{
+				$rows[] = $row;
+			}
+			
+			
+			foreach ($rows as $inputRow)
+			{
+				try
+				{
+					$sth = $dbh->prepare("INSERT INTO Bettors(BettorID, Balance) VALUES (:bettorid, :balance)");
+					
+					$sth->bindValue(":bettorid",$inputRow[0]);
+					$sth->bindValue(":balance", rand(100, 50000));
+
+					$sth->execute();
+					
+					print "Added<br>";
+				}
+				catch (PDOException $e)
+				{
+					printf ("The statement failed.\n");
+					printf ("getCode: ". $e->getCode () . "\n");
+					printf ("getMessage: ". $e->getMessage () . "\n");
+				} 
+			}
+
+			db_close($dbh);
+		?>
+	</body>
+</html>
