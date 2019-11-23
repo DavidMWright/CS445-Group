aaa253031343d3232b77a9d134af7e8ffa49b7d9
DavidMWright <wrig6296@pacificu.edu>
2019-11-23 12:36:17 -0800

    Drop tables script

---
+++ DropAll.sql
@@ -0,0 +1,23 @@
+Drop Table MatchStats;
+Drop Table Starts;
+Drop Table Matchs;
+Drop Table HasAPosition;
+Drop Table Positions;
+Drop Table ParticipatesIn;
+Drop Table League;
+Drop Table Starters;
+Drop Table Cards;
+Drop Table Shots;
+Drop Table Assists;
+Drop Table Goals;
+Drop Table Win;
+Drop Table Players;
+Drop Table MadeBet;
+Drop Table SportsTeams;
+Drop Table Logo;
+Drop Table Bet;
+Drop Table Admins;
+Drop Table Bettors;
+Drop Table Users;
+
+-- Delete from Users;
